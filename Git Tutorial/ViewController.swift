2f574527e939e16211facfb2a1ab2c61889046f6
Wayne Huang <wayne.y.huang@gmail.com>
2020-05-11 18:59:03 -0500

    Initial Commit

---
+++ Git Tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git Tutorial
+//
+//  Created by Wayne Huang on 5/11/20.
+//  Copyright © 2020 Wayne Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
