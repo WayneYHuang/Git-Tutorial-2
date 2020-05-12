bb24794a401ff881f03749f6cc23ff9f53648b43
Wayne Huang <wayne.y.huang@gmail.com>
2020-05-11 22:06:44 -0500

    Added label to storyboard

--- Git Tutorial/ViewController.swift
+++ Git Tutorial/ViewController.swift
@@ -12,11 +12,14 @@ class ViewController: UIViewController {
     
     let message = "Hello Git!"
 
+    @IBOutlet var reverseLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print(reverse(text: "stressed"))
+        let reversed = reverse(text: "stressed")
+        reverseLabel.text = reversed
     }
 
     func reverse(text: String) -> String {
